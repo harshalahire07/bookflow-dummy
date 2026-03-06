997cc7d6db3b4d699e59129a0e90f02e79b8d5e5
vaishh2004 <dokhevaishnavi@gmail.com>
2026-03-06 19:45:33 +0530

    Added homepage template

--- bookflow_dummy/settings.py
+++ bookflow_dummy/settings.py
@@ -54,7 +54,7 @@ ROOT_URLCONF = 'bookflow_dummy.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR / 'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
